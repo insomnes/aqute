4662655bbc38053366cfb69937095b26156b4733
insomnes <denginm@gmail.com>
2023-11-21 22:41:44 +0100

    Add randomized interval rl, update docstrings and tests

--- tests/aqute/test_ratelimiter.py
+++ tests/aqute/test_ratelimiter.py
@@ -6,6 +6,7 @@ import pytest
 from aqute import Aqute
 from aqute.ratelimiter import (
     PerWorkerRateLimiter,
+    RandomizedIntervalRateLimiter,
     SlidingRateLimiter,
     TokenBucketRateLimiter,
 )
@@ -19,16 +20,25 @@ async def non_failing_handler(i: int) -> str:
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     "rl_name",
-    ["sliding", "token", "per_worker"],
-    ids=["Sliding Window", "Token Bucket", "Per Worker Token Bucket"],
+    [
+        "Sliding",
+        "Token Bucket",
+        "Randomized",
+        "Per Worker",
+    ],
 )
 async def test_with_ratelimiter(rl_name: str):
+    # We should initialize them inside running loop
     limiters = {
-        "sliding": SlidingRateLimiter(5, 0.2),
-        "token": TokenBucketRateLimiter(5, 0.2),
+        "Sliding": SlidingRateLimiter(5, 0.2),
+        "Token Bucket": TokenBucketRateLimiter(5, 0.2),
+        # This parameters should ensure passing of this tests
+        "Randomized": RandomizedIntervalRateLimiter(
+            5, 0.2, mean_target_multiplier=0.3, upper_multiplier_bound=0.5
+        ),
         # We have 11 tasks for 10 workers, so per worker rate limiting should allow it
         # at rate of 1 request per 0.4 seconds (for this test to pass)
-        "per_worker": PerWorkerRateLimiter(1, 0.4),
+        "Per Worker": PerWorkerRateLimiter(1, 0.4),
     }
     rate_limiter = limiters[rl_name]
     aqute = Aqute(
